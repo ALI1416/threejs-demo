7ef8412fe8250295e6d2a90f8af6f1cc0a9e9c1f
ALI <1416978277@qq.com>
2022-10-18 17:15:14 +0800

    1018

--- 003-整合gsap/src/App.vue
+++ 003-整合gsap/src/App.vue
@@ -2,7 +2,6 @@
 </template>
 
 <style>
-/*去除白边框*/
 body {
   margin: 0;
 }
@@ -33,10 +32,10 @@ scene.add(cube);
 /* 4、创建渲染器 */
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.append(renderer.domElement);
 
 /* 5、创建控制器 */
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
 /* 6、创建辅助器 */
@@ -44,18 +43,89 @@ const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
 /* 7、gsap动画 */
-// cube沿着x轴方向移动3单位，在5s内
+// 文档 https://greensock.com/docs/v3/GSAP
+// to() https://greensock.com/docs/v3/GSAP/gsap.to()
+// 构造函数
+// 参数名    中文名
+// targets  目标对象
+// vars     参数对象
+// vars参数名  中文名     默认值
+// duration   持续时间    0.5s
+// delay      延迟多少s后执行  0s
+// repeat     重复执行多少次   0(-1无限循环)
+// yoyo       往复运动(需要和repeat配合使用)    false
+// ease       时间曲线    https://greensock.com/get-started#easing
+// onStart()  开始执行回调函数
+// onComplete()   执行完成回调函数
+
+// 7.1、cube沿着x轴方向移动3单位，在5s内
 // cube是Mesh继承于Object3D有属性position是对象Vector3有属性xyz
 // http://localhost:8080/docs/index.html#api/zh/math/Vector3
-// ease设置时间曲线 https://greensock.com/get-started#easing
-gsap.to(cube.position, {x: 3, ease: "bounce.out", duration: 5})
-// 沿着x轴正方向看cube，顺时针旋转一周，在5s内(Math.PI是旋转180度)
+gsap.to(cube.position, {
+  x: 3, // 沿着x轴方向移动3单位
+  duration: 5, // 在5s内
+  ease: "bounce.out", // 时间曲线
+  onStart: () => { // 开始执行回调函数
+    console.log("开始执行")
+  },
+  onComplete: () => { // 执行完成回调函数
+    console.log("执行完成")
+  },
+})
+
+// 7.2、沿着x轴正方向看cube，顺时针旋转一周(Math.PI是旋转180度)，在5s内
 // cube是Mesh继承于Object3D有属性rotation是对象Euler有属性xyz和order
 // http://localhost:8080/docs/index.html#api/zh/math/Euler
-gsap.to(cube.rotation, {x: Math.PI * 2, duration: 5})
-// cube的x的值增加到2单位，在5s内
+let rotateAnimate = gsap.to(cube.rotation, {
+  x: Math.PI * 2, // 顺时针旋转一周
+  duration: 5, // 在5s内
+  repeat: -1, // 无限循环
+  yoyo: true, // 往复运动
+})
+
+// 7.3、cube的x的值增加到2单位，在5s内
 // cube是Mesh继承于Object3D有属性scale是对象Vector3有属性xyz
-gsap.to(cube.scale, {x: 2, duration: 5})
+gsap.to(cube.scale, {
+  x: 2, // x的值增加到2单位
+  duration: 5, // 在5s内
+})
+
+/* 8、监听事件 */
+// 8.1、鼠标单击，开始或暂停旋转
+window.addEventListener("click", () => {
+  // 判断动画是否正在播放
+  if (rotateAnimate.isActive()) {
+    // 暂停
+    rotateAnimate.pause();
+  } else {
+    // 继续播放
+    rotateAnimate.resume()
+  }
+})
+
+// 8.2、鼠标双击，全屏和退出全屏
+window.addEventListener("dblclick", () => {
+  // 判断是否全屏
+  if (document.fullscreenElement) {
+    // 退出全屏
+    document.exitFullscreen();
+  } else {
+    // 动画全屏
+    renderer.domElement.requestFullscreen();
+  }
+})
+
+// 8.3、页面尺寸发生变化，调整画布
+window.addEventListener("resize", () => {
+  // 更新摄像头宽高比
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新摄像头的投影矩阵
+  camera.updateProjectionMatrix();
+  // 更新渲染器尺寸
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 更新渲染器像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+})
 
 function render() {
   renderer.render(scene, camera);
