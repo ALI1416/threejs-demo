4f708e3898b4b49092edb9c0152dbeb9b4e7aa96
ALI <1416978277@qq.com>
2022-10-19 17:37:05 +0800

    1019

---
+++ 005-几何体/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
+
+<script>
+/* 0、安装并导入依赖 */
+import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
+
+/* 1、创建场景 */
+const scene = new THREE.Scene();
+
+/* 2、创建相机 */
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+/* 3、创建物体 */
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({color: 0x66ccff});
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cube);
+// 物体
+console.log(cube.geometry);
+// 几何体 等同于cube.geometry
+console.log(cubeGeometry);
+
+
+/* 4、创建渲染器 */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.append(renderer.domElement);
+
+/* 5、创建控制器 */
+const controls = new OrbitControls(camera, renderer.domElement);
+
+/* 6、创建辅助器 */
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+function render() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
+
+render();
+</script>
